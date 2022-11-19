9b390951c40066832fe3de3fdfbe56e06bbb6c22
Mohammad Hemal <97729902+Cold-Thunder@users.noreply.github.com>
2022-11-19 22:03:57 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+
+let canvas = document.querySelector('canvas');
+
+canvas.height = innerHeight;
+canvas.width = innerWidth;
+
+
+let c = canvas.getContext('2d');
+
+class Circle{
+	constructor( x, y, ra, dx, dy, r, g, b){
+		this.x = x,
+		this.y = y,
+		this.ra = ra,
+		this.dx = dx,
+		this.dy = dy,
+		//color code
+		this.r = r,
+		this.g = g,
+		this.b = b
+	}
+
+	draw(){
+		c.beginPath()
+
+		this.r;
+		this.g;
+		this.b;
+
+		c.fillStyle = `RGB(${this.r}, ${this.g}, ${this.b})`;
+		c.strokeStyle = `RGB(255, 255, 255)`;
+		c.arc(this.x, this.y, this.ra, 0, Math.PI*2, false);
+		c.fill()
+		c.stroke();
+
+	}
+
+	update(){
+
+		if(this.x >= innerWidth - this.ra || this.x < 0){
+		this.dx = -this.dx;
+		}
+
+		if(this.y >= innerHeight - this.ra || this.y < 0){
+			this.dy = -this.dy;
+		}
+
+		this.x += this.dx;
+		this.y += this.dy;
+    }
+
+
+}
+
+
+let cirArr = [];
+
+for(let i = 0; i <= 20; i++){
+	let ra = Math.random()*30;
+	let x = Math.random()* innerWidth;
+	let y = Math.random()* innerHeight;
+	let dx = Math.random()*10;
+	let dy = Math.random()*10;
+//color code start
+	let r; let g; let b;
+	if(x < 255 && x > 0){
+		r = x;
+	}else{
+		r = 255;
+	}
+	if(y < 255 && y > 0){
+		g = y;
+	}else{
+		g = 255;
+	}
+	if(ra < 255 && ra > 0){
+		b = ra;
+	}else{
+		b = 255;
+	}
+//color code end
+	let circle = new Circle(x, y, ra, dx, dy, r, g, b)
+
+	cirArr.push(circle);
+}
+
+
+const anim = () => {
+	requestAnimationFrame(anim);
+	c.clearRect(0,0,innerWidth, innerHeight);
+	
+
+	for(let i = 0; i < cirArr.length; i++){
+		cirArr[i].draw();
+		cirArr[i].update();
+	}
+}
+
+anim()
+
+
+
+
+
+
+
+
+
+
+
+
